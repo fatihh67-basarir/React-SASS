ebf05820239dd8b51f118662926dcd36e2581d28
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-02 16:35:50 +0300

    style change v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import headerStyle from "../scss/header.module.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className={headerStyle["title"]}>Header</h1>
+    </header>
+    
+  )
+}
+
+export default Header
